3cc3e4d73c213bb47fa228d1fc728c9c21a3bde6
Gustavo Duarte <gduartemenezes@gmail.com>
2020-07-14 11:32:02 -0300

    Add content and route to SignUp page

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { FiChevronLeft } from 'react-icons/fi';
+import { Form, Input } from '@rocketseat/unform';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/img/facebook_1.png';
+import { Container, Info } from './styles';
+
+function SignIn() {
+  return (
+    <Container>
+      <Info>
+        <img src={logo} alt="Open Market" />
+        <h3>Join us.</h3>
+        <span>Create your account and enjoy our services.</span>
+        <Link to="/">
+          <FiChevronLeft size={20} color="#FFF" />
+          <span>Back to login</span>
+        </Link>
+      </Info>
+      <Form>
+        <Input type="text" name="name" placeholder="Name" />
+        <Input type="email" name="email" placeholder="E-mail" />
+        <Input type="password" name="password" placeholder="Password" />
+        <button type="button">Sign up</button>
+      </Form>
+    </Container>
+  );
+}
+
+export default SignIn;
--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
+      <Route path="/register" component={SignUp} />
     </Switch>
   );
 }
