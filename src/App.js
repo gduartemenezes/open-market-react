7ca34d3c82f670b86e3c23837ca48bb6c41c4004
Gustavo Duarte <gduartemenezes@gmail.com>
2020-07-14 10:04:54 -0300

    Fix module importation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import {Router} from 'react-router-dom';
-import Routes from './routes';
-import GlobalStyle from './styles/global';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from '~/routes';
+import GlobalStyle from '~/styles/global';
 
 function App() {
   return (
-    <Router>
-      <Routes/>
-      <GlobalStyle/>
-
-
-
-    </Router>
-  )
+    <BrowserRouter>
+      <Routes />
+      <GlobalStyle />
+    </BrowserRouter>
+  );
 }
 
 export default App;
