665ce400ccbcc8ef9aafa49831c9e9de047f6b95
Gustavo Duarte <gduartemenezes@gmail.com>
2020-07-14 10:05:19 -0300

    Fix module importation

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+
+import SignIn from './pages/SignIn';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
     </Switch>
-  )
+  );
 }
