6ad5285cf5bf2d7073daf4a6343f0119c7c3b1a1
Gustavo Duarte <gduartemenezes@gmail.com>
2020-07-14 14:15:35 -0300

    Update global style

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,6 +1,8 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300&display=swap');
+
   *{
     margin: 0;
     outline: 0;
@@ -12,12 +14,23 @@ export default createGlobalStyle`
     outline: 0;
   }
 
+
   body {
-    background: #d5d5d5;
+        -webkit-font-smoothing: antialiased;
+    }
+
+
+  html, body, #root {
+    min-height: 100%;
   }
 
   body, input, button {
-    font: 14px, 'Roboto', sans-serif;
+    font: 14px;
+    font-family: 'Roboto', 'Arial', sans-serif;
+  }
+
+  body {
+    background: #d5d5d5;
   }
 
   a {
