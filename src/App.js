b8ab0843bcff84a04fae40dcd78658b8ea2ec211
Gustavo Duarte <gduartemenezes@gmail.com>
2020-07-15 11:22:10 -0300

    Add ReactotronConfig file to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+
 import Routes from '~/routes';
 import GlobalStyle from '~/styles/global';
+import './config/ReactotronConfig';
 
 function App() {
   return (
