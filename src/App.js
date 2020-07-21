1c5cfafea314b5581237eb8bc11b28e12f335d03
Gustavo Duarte <gduartemenezes@gmail.com>
2020-07-21 10:10:54 -0300

    Create routes folder

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
-import Routes from '~/routes';
-import GlobalStyle from '~/styles/global';
 import './config/ReactotronConfig';
+
+import Routes from '~/routes/index';
+import GlobalStyle from '~/styles/global';
 import store from '~/store';
 
 function App() {
